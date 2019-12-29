ec79d7de29e57d5c5f39c5b5a3c2407e6b3bde1f
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-29 23:30:20 +0100

    Print dllist after loading the artifact

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,6 @@
 using GtkSourceWidget, Test
 using Gtk
 
-@testset "GtkSourceView" begin
+using Libdl
 
-    w = GtkWindow()
-    b = GtkSourceBuffer()
-    b.text[String] = "test"
-    v = GtkSourceView(b)
-    push!(w,v)
-    showall(w)
-    @test b.text[String] == "test"
-
-end
+println(dllist())
